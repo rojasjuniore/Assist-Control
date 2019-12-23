acb1355edfc843ae0a182d711c8c44354dbf32ee
Manuel Fernandez <mfernandezphp@gmail.com>
2019-12-22 21:17:46 -0400

    Resuelto problema con el alert de Red Social

--- resources/views/users/complete_data.blade.php
+++ resources/views/users/complete_data.blade.php
@@ -8,8 +8,8 @@
     <div class="box box-primary">
         <div class="box-body">
             <div class="row">
-                <div class="alert-info text-center">
-                    Se ha detectado el inicio de sesion a traves de una red social, le invitamos a completar su perfil haciendo clic <b><a href="{{route('perfil')}}">aqui</a>.</b>.
+                <div class="alert alert-info text-center">
+                    Se ha detectado el inicio de sesion a traves de una red social, le invitamos a completar su perfil haciendo clic <b><a href="{{route('perfil')}}">aqui</a></b>.
                 </div>
             </div>
         </div>
